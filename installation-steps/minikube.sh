17ae2682b0cf3bbc7429920106f038770ee37351
vikasghandge <ghandgevikas804@gmail.com>
2025-05-13 13:56:41 +0530

    --- Auto Git Commit ---

--- installation-steps/minikube.sh
+++ installation-steps/minikube.sh
@@ -11,5 +11,5 @@ minikube version
 # minikube ip  -- this will print ip
 # minikube start --driver=docker --force
 # kubectl get pods -A --> this will show all pods including namespaces.
-# kubectl get svc 
+# kubectl get svc --> check service 
 # how to access pods --> curl http://<minikube_ip>:<port number>/
